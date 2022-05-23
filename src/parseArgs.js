851ece80972a4050815294a962e3e6caf1c87246
Subhash Yadav <99490164+subhashydv@users.noreply.github.com>
2022-05-23 19:31:07 +0530

    Refactored structureOption
    * Removed loop

--- src/parseArgs.js
+++ src/parseArgs.js
@@ -16,8 +16,8 @@ const isValidValue = (value) => {
 };
 
 const validateOptions = args => {
-  isContainsBothOption(args);
-  for (let index = 0; index < args.length; index += 2) {
+  let index = 0;
+  while (index < args.length) {
     if (!isValidOption(args[index])) {
       throw {
         // eslint-disable-next-line max-len
@@ -29,7 +29,9 @@ const validateOptions = args => {
         message: `head: illegal line count -- ${args[index + 1]}`
       };
     }
+    index += 2;
   }
+  isContainsBothOption(args);
 };
 
 const fileList = args => {
@@ -46,16 +48,13 @@ const structureOption = args => {
   const keys = { '-n': 'line', '-c': 'byte' };
   const options = { switch: 'line', value: 10 };
 
-  let index = 0;
-  while (args[index] === '-n' || args[index] === '-c') {
-    options.switch = keys[args[index]];
-    options.value = args[index + 1];
-    index += 2;
-  }
+  const lastIndex = args.length - 1;
+  options.switch = keys[args[lastIndex - 1]] || options.switch;
+  options.value = args[lastIndex] || options.value;
   return options;
 };
 
-const formatArgs = args => {
+const splitArgs = args => {
   return args.flatMap((arg) => {
     return /^-../.test(arg) ? [arg.slice(0, 2), +arg.slice(2)] : arg;
   });
@@ -67,17 +66,16 @@ const popArgs = (args, argsToPop) => {
 };
 
 const parseArgs = args => {
-  const formatedArgs = formatArgs(args);
-  const files = fileList(formatedArgs);
-  const option = popArgs(formatedArgs, files);
+  const splittedArgs = splitArgs(args);
+  const files = fileList(splittedArgs);
+  const option = popArgs(splittedArgs, files);
   validateOptions(option);
 
-  // isContainsBothOption(option);
-  const structuredOptions = structureOption(formatedArgs);
+  const structuredOptions = structureOption(option);
   return { options: structuredOptions, fileName: files };
 };
 
 exports.parseArgs = parseArgs;
 exports.fileList = fileList;
-exports.formatArgs = formatArgs;
+exports.formatArgs = splitArgs;
 exports.validateOptions = validateOptions;
